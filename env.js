d1868e6f18bfcfbac46081a3a97cd759606a3b03
Evan Powell <evan@evans-mbp-2.home>
2018-09-20 22:16:30 -0500

    add .env template

---
+++ env.js
@@ -0,0 +1,14 @@
+/* 
+FOR LOCAL ENV VARIABLES
+ 1: create a .env file in the root directory
+ 2: copy the code below and paste it into the .env file
+ 3: add the env variables to the .env file
+ 4: DO NOT add the variables in this file
+*/
+
+COSMIC_BUCKET=''
+COSMIC_READ_KEY=''
+COSMIC_WRITE_KEY=''
+
+MAILGUN_DOMAIN=''
+MAILGUN_KEY=''
